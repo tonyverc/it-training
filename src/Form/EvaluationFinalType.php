411906c7ac43d6a336f3d299f21d9a9bc0aa7faf
tonyverc <tony.vercruysse@hotmail.com>
2025-07-25 16:05:29 +0200

    suite formulaire final

--- src/Form/EvaluationFinalType.php
+++ src/Form/EvaluationFinalType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\EvaluationFinal;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -18,18 +20,39 @@ class EvaluationFinalType extends AbstractType
             ->add('equipements')
             ->add('repas')
             ->add('contenuDeLaFormation')
-            ->add('recommandation')
-            ->add('pedagogie')
-            ->add('maitriseDuDomaine')
-            ->add('disponibilite')
-            ->add('reponsesAuxQuestions')
-            ->add('techniquesDanimations', null, [
-                'label' => 'Techniques d\'animations',
+            ->add('recommandation', ChoiceType::class, [
+                'choices' => [
+                    'Oui' => true,
+                    'Non' => false,
+                ],
+                'expanded' => true,
+                'multiple' => false,
             ])
-            ->add('autresProjets')
-            ->add('satisfactionGeneral')
-            ->add('avisFinal')
-            ->add('date')
+            ->add('pedagogie', HiddenType::class, [
+                'required' => true,
+            ])
+            ->add('maitriseDuDomaine', HiddenType::class, [
+                'required' => true,
+            ])
+            ->add('disponibilite', HiddenType::class, [
+                'required' => true,
+            ])
+            ->add('reponsesAuxQuestions', HiddenType::class, [
+                'required' => true,
+            ])
+            ->add('techniquesDanimations', HiddenType::class, [
+                'label' => 'Techniques d\'animations',
+                'required' => true,
+            ])
+            ->add('autresProjets', HiddenType::class, [
+                'required' => true,
+            ])
+            ->add('satisfactionGeneral', HiddenType::class, [
+                'required' => true,
+            ])
+            ->add('avisFinal', HiddenType::class, [
+                'required' => true,
+            ])
             ->add('save', SubmitType::class, [
                 'label' => 'Envoyer l\'évaluation',
             ])
